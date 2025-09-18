a93819d0f466fa299d8c9bafd615471925c3e70d
Tomas Pecukevicius <pecukevicius@gmail.com>
2025-09-18 22:54:51 +0100

    fix: removed eslint and prettier from pre-commit, as they are managed by vite and svelte

--- src/frontend/eslint.config.js
+++ src/frontend/eslint.config.js
@@ -1,49 +1,41 @@
-// src/frontend/eslint.config.js
 import prettier from 'eslint-config-prettier';
 import { fileURLToPath } from 'node:url';
 import { includeIgnoreFile } from '@eslint/compat';
 import js from '@eslint/js';
 import svelte from 'eslint-plugin-svelte';
+import { defineConfig } from 'eslint/config';
 import globals from 'globals';
-import tseslint from 'typescript-eslint';
+import ts from 'typescript-eslint';
 import svelteConfig from './svelte.config.js';
 
 const gitignorePath = fileURLToPath(new URL('./.gitignore', import.meta.url));
 
-export default [
+export default defineConfig(
 	includeIgnoreFile(gitignorePath),
-
-	// Svelte flat config
-	...svelte.configs['flat/recommended'],
-
-	// JS + TS recommended (fast, non type-aware)
 	js.configs.recommended,
-	...tseslint.configs.recommended,
-
-	// Turn off stylistic rules that clash with Prettier
+	...ts.configs.recommended,
+	...svelte.configs.recommended,
 	prettier,
 	...svelte.configs.prettier,
-
-	// Global rules
 	{
 		languageOptions: {
 			globals: { ...globals.browser, ...globals.node }
 		},
 		rules: {
-			'no-undef': 'off' // TS projects shouldn't use this
+			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
+			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
+			'no-undef': 'off'
 		}
 	},
-
-	// Svelte files: enable TS parser inside <script lang="ts">
 	{
 		files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
 		languageOptions: {
 			parserOptions: {
 				projectService: true,
 				extraFileExtensions: ['.svelte'],
-				parser: tseslint.parser,
+				parser: ts.parser,
 				svelteConfig
 			}
 		}
 	}
-];
+);
--- src/frontend/prettier.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	plugins: ['prettier-plugin-svelte', 'prettier-plugin-tailwindcss'],
-	printWidth: 100,
-	singleQuote: false,
-	trailingComma: 'all',
-	overrides: [{ files: '*.svelte', options: { parser: 'svelte' } }]
-};
